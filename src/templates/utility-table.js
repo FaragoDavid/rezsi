f30783d4ecee81563067439e598f4ae1a9607eb2
david-farago <david.farago@emarsys.com>
2026-03-13 19:40:11 +0100

    Make table font size scale with viewport width
    - Use clamp(11px, 2.5vw, 14px) for responsive font sizing
    - Remove fixed font-size overrides in media query
    - Font scales fluidly between 11px-14px based on screen width

--- src/templates/utility-table.js
+++ src/templates/utility-table.js
@@ -1,21 +1,23 @@
 export const utilityTableTemplate = (strings) => `
 <div class="card">
   <h2>${strings.dashboard.heading}</h2>
-  <table id="utility-table">
-      <thead>
-        <tr>
-          <th>${strings.tableDateHeader}</th>
-          <th>${strings.utilities.water}</th>
-          <th>${strings.utilities.gas}</th>
-          <th>${strings.utilities.electricity}</th>
-          <th>${strings.utilities.electricityMain}</th>
-        </tr>
-      </thead>
-      <tbody id="utility-data">
-        <tr>
-          <td colspan="5">${strings.dashboard.loadingMessage}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="table-container">
+    <table id="utility-table">
+        <thead>
+          <tr>
+            <th>${strings.tableDateHeader}</th>
+            <th>${strings.utilities.water}</th>
+            <th>${strings.utilities.gas}</th>
+            <th>${strings.utilities.electricity}</th>
+            <th>${strings.utilities.electricityMain}</th>
+          </tr>
+        </thead>
+        <tbody id="utility-data">
+          <tr>
+            <td colspan="5">${strings.dashboard.loadingMessage}</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
 </div>
 `;
